40e44961d45d71846bef3e7908c2361efb9f3b32
hafedh-hichri <hhichri60@gmail.com>
2023-01-16 13:53:13 +0100

    'final'

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # image-classification-app-using-streamlit
+
+## run streamlit app 
+```
+streamlit run app.py
+```
+
+
+## build the docker image 
+```
+docker build -t my-streamlit-app .
+```
+
+## run the docker image
+```
+docker run -d -p 8501:8501 my-streamlit-app
+```
--- app.py
+++ app.py
@@ -1,76 +1,79 @@
-# # import streamlit as st
-# # import tensorflow as tf
-# # from PIL import Image
-# # import pickle
-# # with open("categories.pickle", 'rb') as f:
-# #     categories = pickle.load(f)
-
-# # # Load your TensorFlow model
-# # model = tf.keras.models.load_model("model.h5")
-
-# # # Create a file uploader widget
-# # uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-
-# # # Show the image
-# # if uploaded_file is not None:
-# #     image = Image.open(uploaded_file)
-# #     st.image(image, caption="Uploaded Image")
-# #     prediction = "none"
-# #     # Use the model to classify the image
-# #     prediction = model.predict(image)
-# #     st.write("Prediction: ", prediction)
-
-
 # import streamlit as st
 # import tensorflow as tf
-# import numpy as np
 # from PIL import Image
 # import pickle
 # with open("categories.pickle", 'rb') as f:
 #     categories = pickle.load(f)
 
-# st.set_option('deprecation.showfileUploaderEncoding', False)
+# # Load your TensorFlow model
+# model = tf.keras.models.load_model("model.h5")
+
+# # Create a file uploader widget
+# uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+# # Show the image
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption="Uploaded Image")
+#     prediction = "none"
+#     # Use the model to classify the image
+#     prediction = model.predict(image)
+#     st.write("Prediction: ", prediction)
 
 
-# @st.cache(allow_output_mutation=True)
-# def load_model():
-# 	model = tf.keras.models.load_model('model.h5')
-# 	return model
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import pickle
+with open("categories.pickle", 'rb') as f:
+    categories = pickle.load(f)
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 
-# def predict_class(image, model):
-
-# 	image = tf.cast(image, tf.float32)
-# 	image = tf.image.resize(image, [224, 224])
-# 	image = np.reshape(image,(1,224,224,-1))
-
-# 	prediction = model.predict(image)
-
-# 	return prediction
+@st.cache(allow_output_mutation=True)
+def load_model():
+	model = tf.keras.models.load_model('model.h5')
+	return model
 
 
-# model = load_model()
+def predict_class(image, model):
 
-# st.title('satelite image clssifier')
+	image = tf.cast(image, tf.float32)
+	image = tf.image.resize(image, [224, 224])
+	image = np.reshape(image, (1, 224, 224, -1))
 
-# file = st.file_uploader("Upload an image of a satellite", type=["jpg", "png"])
+	prediction = model.predict(image)
+
+	return prediction
 
 
-# if file is None:
-# 	st.text('Waiting for upload....')
+model = load_model()
 
-# else:
-# 	slot = st.empty()
-# 	slot.text('Running inference....')
-# 	test_image = Image.open(file).convert("RGB")
-# 	st.image(test_image, caption="Input Image", width=400)
+st.title('satelite image clssifier')
 
-# 	pred = predict_class(np.asarray(test_image), model)
+file = st.file_uploader("Upload an image of a satellite", type=["jpg", "png"])
 
-# 	result = categories[np.argmax(pred)]
 
-# 	output = 'The image is a ' + result
+if file is None:
+	st.text('Waiting for upload....')
 
-# 	slot.text('Done')
+else:
+    slot = st.empty()
+    slot.text('Running inference....')
+    test_image = Image.open(file).convert("RGB")
+    st.image(test_image, caption="Input Image", width=400)
 
-# 	st.success(output)
+    pred = predict_class(np.asarray(test_image), model)
+    print("#############################")
+    print(np.argmax(pred[0]))
+    print(pred)
+    print("#############################")
+    result = categories[np.argmax(pred[0])]
+
+    output = 'The image is ' + result
+
+    slot.text('Done')
+
+    st.success(output)
